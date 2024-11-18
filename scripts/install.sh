d4b9340664dff8f5119400c274d4692822208a58
anthonyb8 <anthonybaxter819@gmail.com>
2024-11-18 14:19:35 -0500

    Included midas-cli in release.

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,9 +4,11 @@ echo "Installing Midas shell ..."
 
 # Copy binaries to /usr/local/bin
 cp bin/midas-shell /usr/local/bin/midas-shell
+cp bin/midas-cli /usr/local/bin/midas-cli
 
 # Make binaries executable
 chmod +x /usr/local/bin/midas-shell
+chmod +x /usr/local/bin/midas-cli
 
 # Ensure ~/.config/midas directory exists
 mkdir -p ~/.config/midas
