a43f89217d21d64c3f11a07aab8192847d419350
Anthony Baxter <97126643+anthonyb8@users.noreply.github.com>
2025-04-30 15:46:16 -0400

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,7 @@
 set -e
 
 REPO_URL="https://github.com/midassystems/midas-shell.git"
-BUILD_DIR="$(mktemp -d -t midas-shell-src-XXXX)"
+BUILD_DIR="$(mktemp -d -t midas-shell-src-XXXXXX)"
 INSTALL_PATH="$HOME/.config/midas/bin"
 
 # Ensure Rust is installed
